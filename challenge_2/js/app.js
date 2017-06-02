57b5623441b547072ee929430a5701f808e82cc9
CamB17 <camcodes@Camerons-MacBook-Pro.local>
2017-06-02 09:59:29 -0600

    Finishes loan calculator

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,34 +1,30 @@
 
 console.log('challenge #2 -- app.js loaded!');
 
- var app = angular.module("appTwo", []);
-//Welcome controller goes here
+var app = angular.module("appTwo", []);
 app.controller("WelcomeController", WelcomeController);
-app.controller("WdiController", WdiController);
  
  function WelcomeController(){
-   this.full_name = "Cameron";
-   this.age = "25";
-   this.city = "Riverside";
-   this.state = "Ca";
-   //this.shout = function (input) {
-   	//	var out = input.UpperCase() + "!";
-   	//	return out;
-
-// };
-}
+   this.full_name = "Cameron",
+   this.age = 25,
+   this.city = "Riverside",
+   this.state = "Ca",
+   this.shout = function(input) {
+   var out = input.toUpperCase();
+    out = out + "!"; 
+    return out;
+  }
+};
 
  function WdiController(){
  	this.class_name = "DenverWdi";
- 	this.enrolled_students = "12";
- 	this.start_date = "March 13th";
- 	this.end_date = "6/02/2017";
+ 	this.enrolled_students = ["Jess", "Alex", "Tyler", "Troy", "Jacy", "Bob", "Tyler", "Cole", "Bill", "Alexi", "Cam", "Nate"],
+ 	this.start_date = "3/13/2017",
+ 	this.end_date = '6/2/2017',
  	//Create countown calculator
- 	this.daysRemaining = () => {
-		let endDate = new Date(2017,06,02);
-		let today = new Date();
-		var oneDay = (24*60*60*1000);
-		var diffDays = Math.round(Math.abs((today.getTime() - endDate.getTime())/(oneDay)));
-		return diffDays;
-	}
-}
+ 	this.daysRemaining = function() {
+      var milliSeconds_left = Date.parse(this.end_date) - Date.now();
+      var days_left = Math.floor(milliSeconds_left/1000/24/60/60);
+      return days_left;
+   }
+};
